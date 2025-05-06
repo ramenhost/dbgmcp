d0c85e74f0c3b1e653471a5248703a53c637c305
Ramkumar <therealramkumar@gmail.com>
2025-05-07 00:38:46 +0530

    docs: add api docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+//! Wrapper over tokio::process::Command to interact with a CLI debugger process.
 use std::ffi::OsStr;
 use std::process::Stdio;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -7,6 +8,8 @@ use tokio::{
     time::{self, Duration},
 };
 
+/// A debugging session that wraps a running CLI debugger process. It abstracts interaction with the inner debugger process.
+/// Use [`CLIDebugger::spawn`] to create a new CLIDebugSession instance.
 pub struct CLIDebugSession {
     child: tokio::process::Child,
     stdin: tokio::process::ChildStdin,
@@ -16,6 +19,7 @@ pub struct CLIDebugSession {
     quit_command: String,
 }
 
+/// A CLI debugger program that when spawned, creates a new [`CLIDebugSession`] instance.
 pub struct CLIDebugger {
     command: tokio::process::Command,
     prompt: Option<String>,
@@ -23,6 +27,7 @@ pub struct CLIDebugger {
 }
 
 impl CLIDebugger {
+    /// Creates a new [`CLIDebugger`] instance from the given program.
     pub fn new<S: AsRef<OsStr>>(program: S) -> Self {
         let command = tokio::process::Command::new(program);
         Self {
@@ -32,6 +37,7 @@ impl CLIDebugger {
         }
     }
 
+    /// Adds arguments to the debugger program.
     #[allow(unused)]
     pub fn args<I, S>(mut self, args: I) -> Self
     where
@@ -42,18 +48,23 @@ impl CLIDebugger {
         self
     }
 
+    /// Sets the prompt used by the debugger program.
+    /// The prompt (example: "(gdb)") is used to keep the interaction in sync with the debugger.
+    /// The default is ">".
     #[allow(unused)]
     pub fn prompt<S: Into<String>>(mut self, prompt: S) -> Self {
         self.prompt = Some(prompt.into());
         self
     }
 
+    /// Sets the command used to quit the debugger program. The default is "quit".
     #[allow(unused)]
     pub fn quit_command<S: Into<String>>(mut self, quit_command: S) -> Self {
         self.quit_command = Some(quit_command.into());
         self
     }
 
+    /// Start a new debugger session. The Ok value returned is a [`CLIDebugSession`] instance that corresponds to the spawned debugger process.
     pub fn spawn(mut self) -> Result<CLIDebugSession, std::io::Error> {
         let mut child = self
             .command
@@ -67,7 +78,7 @@ impl CLIDebugger {
             stdout: tokio::io::BufReader::new(child.stdout.take().unwrap()),
             stderr: tokio::io::BufReader::new(child.stderr.take().unwrap()),
             child,
-            prompt: self.prompt.unwrap_or(String::from("$")),
+            prompt: self.prompt.unwrap_or(String::from(">")),
             quit_command: self.quit_command.unwrap_or(String::from("quit")),
         })
     }
@@ -75,6 +86,7 @@ impl CLIDebugger {
 
 const CHILD_READ_TIMEOUT: Duration = Duration::from_secs(10);
 impl CLIDebugSession {
+    /// Send a command to the inner debugger process followed by a newline.
     pub async fn send_command(&mut self, command: &str) -> Result<(), std::io::Error> {
         self.stdin.write_all(command.as_bytes()).await?;
         self.stdin.write_u8(b'\n').await?;
@@ -82,6 +94,7 @@ impl CLIDebugSession {
         Ok(())
     }
 
+    /// Read the response from the inner debugger process until a pattern is matched or a timeout occurs.
     pub async fn read_response_until<S: AsRef<str>>(
         &mut self,
         pattern: Option<S>,
@@ -128,11 +141,15 @@ impl CLIDebugSession {
         }
     }
 
+    /// Read the response from the inner debugger process until next prompt appears.
+    /// This function can timeout if no prompt is received within the default timeout (10s).
     pub async fn read_response(&mut self) -> Result<String, std::io::Error> {
         self.read_response_until::<&str>(None, CHILD_READ_TIMEOUT)
             .await
     }
 
+    /// Send a command to the inner debugger process and read the response until next prompt appears.
+    /// This function can timeout if no prompt is received within the default timeout (10s).
     pub async fn execute_command<S: AsRef<str>>(
         &mut self,
         command: S,
@@ -153,6 +170,7 @@ impl CLIDebugSession {
         Ok(response)
     }
 
+    /// Gracefully terminate the inner debugger process.
     pub async fn terminate(&mut self) -> Result<(), std::io::Error> {
         self.send_command(self.quit_command.clone().as_str())
             .await?;
@@ -161,6 +179,7 @@ impl CLIDebugSession {
     }
 }
 
+/// Get a unique number for debugger session identifier.
 pub fn generate_session_id() -> u32 {
     static SESSION_ID: AtomicUsize = AtomicUsize::new(0);
     SESSION_ID.fetch_add(1, Ordering::Relaxed) as u32
